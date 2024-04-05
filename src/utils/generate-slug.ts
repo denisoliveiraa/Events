e49328ac63b8f67cac452b0d85bc7e71b5cb459f
denisoliveiraa <denis_o@hotmail.com.br>
2024-04-04 22:32:10 -0300

    adding slug

---
+++ src/utils/generate-slug.ts
@@ -0,0 +1,8 @@
+export function generateSLu(text: string): string {
+  return text
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .toLowerCase()
+      .replace(/[^\w\s\-]/g, "")
+      .replace(/\s+/g, "-");
+}
